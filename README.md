a8a614f2b83b817f2bc8e4c5180df2f5727be06a
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-09-25 00:33:08 +0100

    initial commit
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+# hook-proxy
+Simple hook for integrating 3rd-party APIs
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
---
+++ index.js
@@ -0,0 +1,56 @@
+"use strict";
+const restify = require('restify');
+const request = require('request');
+
+const server = restify.createServer({
+    name: "proxis"
+});
+
+
+server.use(restify.bodyParser());
+
+server.post('/hooks/codacy/twilio', (req, res, next) => {
+    const TRELLO_KEY = process.env.TRELLO_KEY;
+
+    try {
+
+        if (req.body) {
+            var requestBody = req.body;
+
+            if (requestBody.result) {
+                speech = '';
+
+                if (requestBody.result.fulfillment) {
+                    speech += requestBody.result.fulfillment.speech;
+                    speech += ' ';
+                }
+
+                if (requestBody.result.action) {
+                    speech += 'action: ' + requestBody.result.action;
+                }
+            }
+        }
+
+        res.send({
+            speech: speech,
+            displayText: speech,
+            source: 'telemundo'
+        });
+
+    } catch (err) {
+
+        res.send(400, {
+            status: {
+                code: 400,
+                errorType: err.message
+            }
+        });
+
+    }
+
+    return next();
+});
+
+server.listen(process.env.PORT || 5000, function() {
+    console.log('%s listening', server.name);
+});
