88ad16b32e58aa0500d5121673c7299c1214fce6
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-09-25 01:34:42 +0100

    update
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

--- index.js
+++ index.js
@@ -10,13 +10,22 @@ const server = restify.createServer({
 server.use(restify.bodyParser());
 
 server.post('/hooks/codacy/trello', (req, res, next) => {
-    const TRELLO_KEY = process.env.TRELLO_KEY;
-    const TRELLO_TOKEN = process.env.TRELLO_TOKEN;
-
+    const TRELLO_API_KEY = process.env.TRELLO_KEY;
+    const TRELLO_API_TOKEN = process.env.TRELLO_TOKEN;
+    const TRELLO_API_URL = `https://api.trello.com/1/cards?key=${TRELLO_API_KEY}&token=${TRELLO_API_TOKEN}`;
     const CALLBACK_URL = `https://${req.headers.host}/callbacks/trello`;
-    console.log(req.params);
+    const BOARD_LIST_ID = '5ba6a9f407c57d6b9cc105c2'
 
-    res.send(CALLBACK_URL);
+    console.log(req.params);
+    const payload = {
+        name: '',
+        callbackURL: CALLBACK_URL,
+        desc: '',
+        pos: 'top',
+        idList: BOARD_LIST_ID,
+        idLabels: 'Bug',
+    }
+    res.send(TRELLO_API_URL);
     return next();
 });
 
